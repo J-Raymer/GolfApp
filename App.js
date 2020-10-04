05e1b75edf45d2fdfcec904e8a8b7bb26b22430e
Joseph Raymer <jraymer@uvic.ca>
2020-10-03 17:01:32 -0700

    Initial commit

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableNativeFeedback,
+  TouchableOpacity, TouchableHighlight
+} from 'react-native';
 
 export default function App() {
+  console.log(require("./assets/icon.png"));
+
+
   return (
-    <View style={styles.container}>
-      <Text>My Golf App </Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Golf App
+      </Text>
+      <TouchableOpacity onPress={() => console.log("Image tapped")}>
+        <View style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}></View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => console.log("photo tapped")}>
+        <Image source={{
+          height: 300,
+          width: 200,
+          uri: "https://picsum.photos/200/300",
+        }} />
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
+
   );
 }
 
